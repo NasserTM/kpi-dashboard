d9bc0d7e39fe71378c95d09f99ee0c0b7d06d540
Daniel Givens <daniel.givens@rackspace.com>
2015-09-17 12:03:20 -0500

    Added some config options

--- kpi_dash/__init__.py
+++ kpi_dash/__init__.py
@@ -3,11 +3,11 @@ from flask import Flask
 
 app = Flask(__name__)
 
-app.config['DEBUG'] = True
-
 with open('config.yml', 'r') as stream:
     metrics_config = yaml.load(stream)
 
+app.config['SECRET_KEY'] = metrics_config['secret_key']
+app.config['DEBUG'] = metrics_config['debug']
 app.config['GRAPHITE_SERVER'] = metrics_config['graphite']
 app.config['brand'] = metrics_config['brand']
 app.config['regions'] = metrics_config['regions']
